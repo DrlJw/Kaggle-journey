1e4d5d4fe90a0dccefb04b735507effb9b40a1a8
DrlJw <36078405+DrlJw@users.noreply.github.com>
2018-08-04 20:48:31 +0800

    adding some features

--- New York City Taxi Trip Duration/New York City Taxi Trip Duration.py
+++ New York City Taxi Trip Duration/New York City Taxi Trip Duration.py
@@ -33,48 +33,26 @@ train = train.drop(train[(train['pickup_longitude'] < -74.05)].index)
 train = train.drop(train[(train['pickup_longitude'] > -73.7)].index)
 city_long_border = (-74.05, -73.7)
 city_lat_border = (40.6, 40.9)
-# 上车地点经纬度可视化
-# fig, ax = plt.subplots(ncols=2, sharex=True, sharey=True)
-# ax[0].scatter(train['pickup_longitude'].values[:500000], train['pickup_latitude'].values[:500000],
-#               color='blue', s=1, label='train',marker='o',alpha=0.2)
-# ax[1].scatter(test['pickup_longitude'].values[:500000], test['pickup_latitude'].values[:500000],
-#               color='green', s=1, label='test',marker='o',alpha=0.2)
-# fig.suptitle('Train and test area complete overlap.')
-# ax[0].legend(loc=0)
-# ax[0].set_ylabel('latitude')
-# ax[0].set_xlabel('longitude')
-# ax[1].set_xlabel('longitude')
-# ax[1].legend(loc=0)
-# plt.ylim(city_lat_border)
-# plt.xlim(city_long_border)
-# plt.show()
 
-coords = np.vstack((train[['pickup_latitude', 'pickup_longitude']].values,
-                    train[['dropoff_latitude', 'dropoff_longitude']].values))
-
+# 上车地点聚类，各路段拥堵情况不同，并加上虚拟变量
+coord_pickup = np.vstack((train[['pickup_latitude', 'pickup_longitude']].values,
+                          test[['pickup_latitude', 'pickup_longitude']].values))
+coord_dropoff = np.vstack((train[['dropoff_latitude', 'dropoff_longitude']].values,
+                           test[['dropoff_latitude', 'dropoff_longitude']].values))
+coords = np.hstack((coord_pickup,coord_dropoff))# 4 dimensional data
 sample_ind = np.random.permutation(len(coords))[:500000]
-kmeans = MiniBatchKMeans(n_clusters=100, batch_size=10000).fit(coords[sample_ind])
+kmeans = MiniBatchKMeans(n_clusters=20, batch_size=10000).fit(coords[sample_ind])
+for df in (train,test):
+    df.loc[:, 'pickup_dropoff_loc'] = kmeans.predict(df[['pickup_latitude', 'pickup_longitude',
+                                                         'dropoff_latitude','dropoff_longitude']])
 
-# 上车地点聚类，各路段拥堵情况不同
-train.loc[:, 'pickup_cluster'] = kmeans.predict(train[['pickup_latitude', 'pickup_longitude']])
-train.loc[:, 'dropoff_cluster'] = kmeans.predict(train[['dropoff_latitude', 'dropoff_longitude']])
-test.loc[:, 'pickup_cluster'] = kmeans.predict(test[['pickup_latitude', 'pickup_longitude']])
-test.loc[:, 'dropoff_cluster'] = kmeans.predict(test[['dropoff_latitude', 'dropoff_longitude']])
+kmean_train= pd.get_dummies(train['pickup_dropoff_loc'],prefix='loc', prefix_sep='_')
+kmean_test= pd.get_dummies(test['pickup_dropoff_loc'],prefix='loc', prefix_sep='_')
+train  = pd.concat([train,kmean_train],axis=1)
+test= pd.concat([test,kmean_test],axis=1)
 
-# train.loc[:, 'center_latitude'] = (train['pickup_latitude'].values + train['dropoff_latitude'].values) / 2
-# train.loc[:, 'center_longitude'] = (train['pickup_longitude'].values + train['dropoff_longitude'].values) / 2
-# test.loc[:, 'center_latitude'] = (test['pickup_latitude'].values + test['dropoff_latitude'].values) / 2
-# test.loc[:, 'center_longitude'] = (test['pickup_longitude'].values + test['dropoff_longitude'].values) / 2
 
-# fig, ax = plt.subplots(ncols=1, nrows=1)
-# ax.scatter(train.pickup_longitude.values[:500000], train.pickup_latitude.values[:500000], s=10, lw=0,
-#            c=train.pickup_cluster[:500000].values, cmap='autumn', alpha=0.2)
-# ax.set_xlim(city_long_border)
-# ax.set_ylim(city_lat_border)
-# ax.set_xlabel('Longitude')
-# ax.set_ylabel('Latitude')
-# plt.show()
-
+# 圆上两点距离
 def haversine_array(lat1, lng1, lat2, lng2):
     lat1, lng1, lat2, lng2 = map(np.radians, (lat1, lng1, lat2, lng2))
     AVG_EARTH_RADIUS = 6371  # in km
@@ -85,7 +63,6 @@ def haversine_array(lat1, lng1, lat2, lng2):
     return h
 
 
-# 圆上两点距离
 # 两点间曼哈顿距离
 def dummy_manhattan_distance(lat1, lng1, lat2, lng2):
     a = haversine_array(lat1, lng1, lat1, lng2)
@@ -148,35 +125,16 @@ weather_event = pd.Series(pd.to_datetime(weather_event, format = '%Y%m%d')).dt.d
 train['extreme_weather'] = train.pickup_date.isin(weather_event).map({True: 1, False: 0})
 test['extreme_weather'] = test.pickup_date.isin(weather_event).map({True: 1, False: 0})
 
-# train.loc[:, 'avg_speed_h'] = 1000 * train['distance_haversine'] / train['trip_duration']
-# train.loc[:, 'avg_speed_m'] = 1000 * train['distance_dummy_manhattan'] / train['trip_duration']
-# fig, ax = plt.subplots(ncols=3, sharey=True)
-# ax[0].plot(train.groupby('Hour').mean()['avg_speed_m'], 'bo-', lw=2, alpha=0.7)
-# ax[1].plot(train.groupby('dayofweek').mean()['avg_speed_m'], 'go-', lw=2, alpha=0.7)
-# ax[2].plot(train.groupby('Month').mean()['avg_speed_m'], 'ro-', lw=2, alpha=0.7)
-# ax[0].set_xlabel('Hour of Day')
-# ax[1].set_xlabel('Day of Week')
-# ax[2].set_xlabel('Month of Year')
-# ax[0].set_ylabel('Average Speed')
-# fig.suptitle('Average Traffic Speed by Date-part')
-# plt.show()
+# 地点与速度聚类，时间段与速度聚类
+train.loc[:, 'avg_speed_h'] = 1000 * train['distance_haversine'] / train['trip_duration']
+train.loc[:, 'avg_speed_m'] = 1000 * train['distance_dummy_manhattan'] / train['trip_duration']
 
-# print(train['pickup_date'].head())# 2016-03-14
-# print(train['pickup_datetime'].head())# 2016-03-14 17:24:55
-# print(train.groupby('passenger_count').size()) # 乘客数量的数量分布
-# print(test.groupby('passenger_count').size())
-
-# YN = train[["store_and_fwd_flag", "trip_duration"]].groupby(['store_and_fwd_flag'],as_index=False).mean()
-# print(YN)   # N958 Y1080
-
-# passenegerNUM = train[["passenger_count", "trip_duration"]].groupby(['passenger_count'],as_index=False).mean()
-# print(passenegerNUM)
-# print(train[(train['passenger_count']==0)]['trip_duration'])
-
-# plt.hist(train['log_trip_duration'].values, bins=100)
-# plt.xlabel('log(trip_duration)')
-# plt.ylabel('number of train records')
-# plt.show()
+for gby_col in ['Hour', 'hourofweek',
+                'dayofweek', 'pickup_dropoff_loc']:
+    gby = train.groupby(gby_col).mean()[['avg_speed_h', 'avg_speed_m']]
+    gby.columns = ['%s_gby_%s' % (col, gby_col) for col in gby.columns]
+    train = pd.merge(train, gby, how='left', left_on=gby_col, right_index=True)
+    test = pd.merge(test, gby, how='left', left_on=gby_col, right_index=True)
 
 fr1 = pd.read_csv(
     'E:/files/Kaggle/New York City Taxi Trip Duration/new-york-city-taxi-with-osrm/fastest_routes_train_part_1.csv',
@@ -191,16 +149,57 @@ train_street_info = pd.concat((fr1, fr2))
 train = train.merge(train_street_info, how='left', on='id')
 test = test.merge(test_street_info, how='left', on='id')
 
+
+# 不知道什么意思的特征
+for gby_cols in [['Hour', 'pickup_dropoff_loc']]:
+    coord_speed = train.groupby(gby_cols).mean()[['avg_speed_h']].reset_index()
+    coord_count = train.groupby(gby_cols).count()[['id']].reset_index()
+    coord_stats = pd.merge(coord_speed, coord_count, on=gby_cols)
+    coord_stats = coord_stats[coord_stats['id'] > 100]
+    coord_stats.columns = gby_cols + ['avg_speed_h_%s' % '_'.join(gby_cols), 'cnt_%s' %  '_'.join(gby_cols)]
+    train = pd.merge(train, coord_stats, how='left', on=gby_cols)
+    test = pd.merge(test, coord_stats, how='left', on=gby_cols)
+
+group_freq = '60min'
+df_all = pd.concat((train, test))[['id', 'pickup_datetime', 'pickup_dropoff_loc']]
+train.loc[:, 'pickup_datetime_group'] = train['pickup_datetime'].dt.round(group_freq)
+test.loc[:, 'pickup_datetime_group'] = test['pickup_datetime'].dt.round(group_freq)
+
+dropoff_counts = df_all \
+    .set_index('pickup_datetime') \
+    .groupby([pd.TimeGrouper(group_freq), 'pickup_dropoff_loc']) \
+    .agg({'id': 'count'}) \
+    .reset_index().set_index('pickup_datetime') \
+    .groupby('pickup_dropoff_loc').rolling('240min').mean() \
+    .drop('pickup_dropoff_loc', axis=1) \
+    .reset_index().set_index('pickup_datetime').shift(freq='-120min').reset_index() \
+    .rename(columns={'pickup_datetime': 'pickup_datetime_group', 'id': 'dropoff_cluster_count'})
+
+train['dropoff_cluster_count'] = train[['pickup_datetime_group', 'pickup_dropoff_loc']].merge(dropoff_counts, on=['pickup_datetime_group', 'pickup_dropoff_loc'], how='left')['dropoff_cluster_count'].fillna(0)
+test['dropoff_cluster_count'] = test[['pickup_datetime_group', 'pickup_dropoff_loc']].merge(dropoff_counts, on=['pickup_datetime_group', 'pickup_dropoff_loc'], how='left')['dropoff_cluster_count'].fillna(0)
+
+
+coords = np.vstack((train[['pickup_latitude', 'pickup_longitude']].values,
+                    train[['dropoff_latitude', 'dropoff_longitude']].values,
+                    test[['pickup_latitude', 'pickup_longitude']].values,
+                    test[['dropoff_latitude', 'dropoff_longitude']].values))
+
+pca = PCA()
+pca.fit(coords)
+train['pickup_pca0'] = pca.transform(train[['pickup_latitude', 'pickup_longitude']])[:, 0]
+train['pickup_pca1'] = pca.transform(train[['pickup_latitude', 'pickup_longitude']])[:, 1]
+train['dropoff_pca0'] = pca.transform(train[['dropoff_latitude', 'dropoff_longitude']])[:, 0]
+train['dropoff_pca1'] = pca.transform(train[['dropoff_latitude', 'dropoff_longitude']])[:, 1]
+test['pickup_pca0'] = pca.transform(test[['pickup_latitude', 'pickup_longitude']])[:, 0]
+test['pickup_pca1'] = pca.transform(test[['pickup_latitude', 'pickup_longitude']])[:, 1]
+test['dropoff_pca0'] = pca.transform(test[['dropoff_latitude', 'dropoff_longitude']])[:, 0]
+test['dropoff_pca1'] = pca.transform(test[['dropoff_latitude', 'dropoff_longitude']])[:, 1]
+train.loc[:, 'pca_manhattan'] = np.abs(train['dropoff_pca1'] - train['pickup_pca1']) + np.abs(train['dropoff_pca0'] - train['pickup_pca0'])
+test.loc[:, 'pca_manhattan'] = np.abs(test['dropoff_pca1'] - test['pickup_pca1']) + np.abs(test['dropoff_pca0'] - test['pickup_pca0'])
+
 # _ = input('Press [Enter] to continue.')
 
 # ----------------------------------------------- 训练模型并评价 ---------------------------------------------------------
-
-y_train = train['log_trip_duration']
-selected_features = ['distance_haversine', 'direction', 'dayofweek', 'Hour', 'Month', 'pickup_cluster',
-                     'total_distance', 'total_travel_time', 'number_of_steps','extreme_weather']
-X_train = train[selected_features]
-X_test = test[selected_features]
-
 
 def regressor_moduel(estimator, X_train, y_train, X_train1, X_cv, y_train1, y_cv):
     print('cross_val_score: ', end='')
@@ -223,10 +222,32 @@ def evalerror(preds, dtrain):
     labels = dtrain.get_label()
     return 'RMSLE', np.sqrt(mean_squared_log_error(preds, labels))
 
+y_train = train['log_trip_duration']
+# selected_features = ['distance_haversine', 'distance_dummy_manhattan', 'direction',
+#                      'extreme_weather', 'vendor_id','total_travel_time','total_distance','number_of_steps',
+#                      'avg_speed_m_gby_Hour', 'avg_speed_m_gby_hourofweek',
+#                      'avg_speed_m_gby_dayofweek', 'avg_speed_m_gby_pickup_dropoff_loc']
+do_not_use_for_training = ['id', 'log_trip_duration', 'pickup_datetime', 'dropoff_datetime',
+                           'pickup_latitude', 'pickup_longitude','dropoff_latitude', 'dropoff_longitude',
+                           'trip_duration', 'store_and_fwd_flag','passenger_count',
+                           'pickup_cluster','dropoff_cluster',
+                           'pickup_date', 'avg_speed_h', 'avg_speed_m',
+                           'pickup_lat_bin', 'pickup_long_bin',
+                           'center_lat_bin', 'center_long_bin',
+                           'pickup_dt_bin', 'pickup_datetime_group']
+selected_features = [f for f in train.columns if f not in do_not_use_for_training]
+X_train = train[selected_features]
+X_test = test[selected_features]
 
 X_train=X_train.values # dataframe to numpy.array
 y_train=y_train.values
 X_test=X_test.values
+
+print(len(selected_features))
+print(X_train.shape,X_test.shape)
+print(X_train.columns.values.tolist())
+
+_ = input('Press [Enter] to continue.')
 
 kf = KFold(n_splits=10)
 kf.get_n_splits(X_train)
@@ -236,15 +257,17 @@ for train_index, test_index in kf.split(X_train):
     X_kf_train, X_kf_test = X_train[train_index], X_train[test_index]
     y_kf_train, y_kf_test = y_train[train_index], y_train[test_index]
 
-dtrain = xgb.DMatrix(X_kf_train, label=y_kf_train)
-dvalid = xgb.DMatrix(X_kf_test, label=y_kf_test)
+X_train1, X_cv, y_train1, y_cv = train_test_split(X_train, y_train, test_size=0.2, random_state=33)
+    
+dtrain = xgb.DMatrix(X_train1, label=y_train1)
+dvalid = xgb.DMatrix(X_cv, label=y_cv)
 dtest = xgb.DMatrix(X_test)
 watchlist = [(dtrain, 'train'), (dvalid, 'valid')]
 
 # Try different parameters! My favorite is random search :)
-xgb_pars = {'min_child_weight': 10, 'eta': 0.04, 'colsample_bytree': 0.8, 'max_depth': 15,
-            'subsample': 0.75, 'lambda': 2, 'nthread': -1, 'booster' : 'gbtree', 'silent': 1, 'gamma' : 0,
-            'objective': 'reg:linear', 'eval_metric': 'rmse'}
+xgb_pars = {'min_child_weight': 10, 'eta': 0.05, 'colsample_bytree': 0.3, 'max_depth': 12,
+            'subsample': 0.8, 'lambda': 0.2, 'nthread': -1, 'booster': 'gbtree', 'silent': 1,
+            'gamma': 0.05, 'objective': 'reg:linear', 'eval_metric': 'rmse'}
 
 xgb_model = xgb.train(xgb_pars, dtrain, 200, watchlist, early_stopping_rounds=250,
                   maximize=False, verbose_eval=15)
